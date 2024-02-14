9f6fb404f9e5cc8abb67c2b9399716bb9b2bae8b
Haleem Hawkins <62164901+haleemhawkins@users.noreply.github.com>
2024-02-14 00:29:34 +0000

    created user table/db rename

--- create_table.sql
+++ create_table.sql
@@ -1,9 +1,15 @@
 -- SQLite
 
-CREATE TABLE [ProductData] (
-ProductID int,
-ProductName string,
-ProductImg VARBINARY(1000), 
-Tags string
+CREATE TABLE [Product] (
+    ProductID int,
+    ProductName string,
+    ProductImg VARBINARY(1000), 
+    Tags string
+);
 
+CREATE TABLE [User] (
+    UserID uint,
+    UserName string,
+    Password int,
+    isAdmin bool
 );
